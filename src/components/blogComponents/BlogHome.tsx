594d242a73909f17e2884e0cdda912cfbdcaeb7d
Ashik Api <ashikapi65@gmail.com>
2026-02-19 16:03:42 +0600

    update blog section

--- src/components/blogComponents/BlogHome.tsx
+++ src/components/blogComponents/BlogHome.tsx
@@ -1,11 +1,11 @@
 const BlogHome = () => {
   return (
     <div>
-        <div className='h-screen bg-[url("https://websitedemos.net/clothing-store-04/wp-content/uploads/sites/1538/2025/05/blog-hero.jpg")] bg-cover bg-center bg-no-repeat flex justify-center items-center text-center'>
-            <div className='flex justify-center items-center'>
-            <div className='text-left space-y-4 ml-210 mt-10'>
-                <h1 className='text-6xl font-bold text-black space-y-4 gap-4'>Style, Tips & <p className='mt-4'> More</p></h1>
-                <p className='text-left text-lg'>Welcome to The Velora Edit, your go-to destination for all things fashion.</p>
+        <div className='relative w-full lg:h-screen h-[800px] bg-[url("https://websitedemos.net/clothing-store-04/wp-content/uploads/sites/1538/2025/05/blog-hero.jpg")] lg:bg-cover lg:bg-center bg-top-left bg-no-repeat flex justify-center items-center text-center'>
+            <div className='lg:absolute right-0 flex justify-end items-center'>
+            <div className='lg:w-xl w-full lg:text-left text-center space-y-4 mt-10'>
+                <h1 className='md:text-7xl text-3xl font-bold text-black space-y-4 gap-4'>Style, Tips & <p className='mt-4'> More</p></h1>
+                <p className='text-left text-[16px]'>Welcome to The Velora Edit, your go-to destination for all things fashion.</p>
             </div>
             </div>
         </div>
